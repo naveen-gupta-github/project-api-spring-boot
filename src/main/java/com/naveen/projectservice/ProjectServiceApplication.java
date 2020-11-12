eaa61b60136577f1b6bd31ad2655df28421e5fd7
naveen-gupta-github <ng3870698@gmail.com>
2020-11-12 13:51:19 +0530

    added oauth2 security

--- src/main/java/com/naveen/projectservice/ProjectServiceApplication.java
+++ src/main/java/com/naveen/projectservice/ProjectServiceApplication.java
@@ -3,9 +3,11 @@ package com.naveen.projectservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 //(exclude = {DataSourceAutoConfiguration.class })
+@EnableResourceServer
 public class ProjectServiceApplication {
 
 	public static void main(String[] args) {
